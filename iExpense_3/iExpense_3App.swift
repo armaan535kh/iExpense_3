ecb70bcf537499d02fa446bdd81d7e4219a95543
armaan535kh <kh.armaan535@gmail.com>
2024-08-02 21:50:56 -0700

    Initial Commit

---
+++ iExpense_3/iExpense_3App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_3App.swift
+//  iExpense_3
+//
+//  Created by Armaan Khan  on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
